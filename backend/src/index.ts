bcc15bd973b30dae5c39d2dc80aa89f03035b9b5
Prakhar Upadhyay <prakharupadhyay407@gmail.com>
2026-02-26 23:23:43 +0530

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,12 +33,4 @@ app.use("/api/comments", commentRoutes);
 
 
 
-const port = ENV.PORT || 3000;
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(port, () => {
-    console.log(`Server is up and running on PORT: ${port}`);
-  });
-}
-
-export default app;
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
